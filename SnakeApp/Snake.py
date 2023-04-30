17787e0a812f5cf8e42e59ed96ca862c45fd24df
Veronika Vronskaya <vveronika1917@gmai.com>
2023-04-30 23:45:51 +0300

    Добавила движение змеи по нажатию кнопок

--- SnakeApp/Snake.py
+++ SnakeApp/Snake.py
@@ -2,77 +2,75 @@ from tkinter import *
 from tkinter import messagebox
 
 class Snake_element:
-    def __init__(self, x, y, half_width_x, half_width_y = None):
+    def __init__(self, x, y, width_x, width_y = None):
+        self.id = None
         self.x = x
         self.y = y
-        self.half_width_x = half_width_x
-        self.half_width_y = half_width_x if half_width_y == None else half_width_y
-        #self.draw()
+        self.width_x = width_x
+        self.width_y = width_x if width_y == None else width_y
 
     def draw(self):
-        self.id = canvas.create_oval(self.coords(),
+        if self.id == None:
+            self.id = canvas.create_oval(self.coords(),
                             fill='black',
                             activefill='red')
+        else:
+            canvas.coords(self.id, self.coords())    
         
     def coords(self):
-        coords = (self.x - self.half_width_x,
-                self.y - self.half_width_y,
-                self.x + self.half_width_x,
-                self.y + self.half_width_y)
+        coords = (self.x - self.width_x / 2,
+                self.y - self.width_y / 2,
+                self.x + self.width_x / 2,
+                self.y + self.width_y / 2)
         return coords
 
     def move(self, d_x = 0, d_y = 0):
         self.x = self.x + d_x
         self.y = self.y + d_y
-        canvas.coords(self.id, self.coords())    
+        self.draw()
 
 class Snake:
     def __init__(self, lenght, start_x, start_y):
-        self.d_x = 20
+        self.width_x = 20
+        self.width_y = 20
+        self.d_x = self.width_x
         self.d_y = 0
         x = start_x
         y = start_y
         self.lenght = lenght
         self.body = []
         for i in range(lenght):
-            element = Snake_element(x, y, 10)
+            element = Snake_element(x, y, self.width_x, self.width_y)
             self.body.append(element)
-            x -= 2 * element.half_width_x
-
-    def draw(self):
-         for i in self.body:
-            i.draw()
-
+            x -= element.width_x
 
     def move(self):
-        for i in self.body:
-            i.move(self.d_x, self.d_y)
-
-def up(event):
-    b.d_x = 0
-    b.d_y -= 20
+        for i in range(len(self.body) - 1, 0, -1):
+            self.body[i].x = self.body[i - 1].x
+            self.body[i].y = self.body[i - 1].y
+            self.body[i].draw()    
+        self.body[0].x = self.body[0].x + self.d_x
+        self.body[0].y = self.body[0].y + self.d_y
+        self.body[0].draw()
+        
+def key_pressed(event):
+    key = event.keysym
+    if key == 'Up':
+        b.d_x = 0
+        b.d_y = -b.width_y
+    if key == 'Down':
+        b.d_x = 0
+        b.d_y = b.width_y
+    if key == 'Left':
+        b.d_x = -b.width_x
+        b.d_y = 0
+    if key == 'Right':
+        b.d_x = b.width_x
+        b.d_y = 0
 
 def for_closing():
     if messagebox.askokcancel(title = 'Выход', message='Вы хотите закрыть приложение?'):
         root.destroy()
-
-def size(id):
-    (leftX, topY, rightX, bottomY) = canvas.coords(id)
-    size_x = (rightX - leftX) / 2
-    size_y = (bottomY - topY) / 2
-    return size_x, size_y 
-
-def mouse_coords():
-    x = canvas.winfo_pointerx() - canvas.winfo_rootx()
-    y = canvas.winfo_pointery() - canvas.winfo_rooty()
-    return x, y
-
-def move(event):
-    id = canvas.find_withtag('current')
-    if id != ():
-        x, y = mouse_coords()
-        size_x, size_y = size(id)
-        canvas.coords(id, x-size_x, y-size_y, x+size_x, y+size_y)
 
 def main():
     b.move()
@@ -80,24 +78,21 @@ def main():
 
 root = Tk()
 
-root.title('Точки')
+root.title('Питон')
 root.geometry('400x400')
 root.resizable(False, False)
-root.iconbitmap(default='C:\Prog\SnakeApp\Snake1.jpg')
+#root.iconbitmap(default='C:\Prog\SnakeApp\Snake1.jpg')
 
 canvas = Canvas(root, width=390, height=390,  cursor = 'heart')
 canvas.pack()
 
-canvas.bind('<B1-Motion>', move)
-
-root.bind('<Up>', up)
+root.bind('<Key>', key_pressed)
 
 root.protocol('WM_DELETE_WINDOW', for_closing)
 
 canvas.focus()
 
 b = Snake(10, 200, 200)
-b.draw()
 
 main()
 
