48cdd93686e46164929818de0e67f99d3e7d6e31
Veronika Vronskaya <vveronika1917@gmai.com>
2023-08-23 22:13:55 +0300

    Еще один коммит

--- app_by_kivy/figures.py
+++ app_by_kivy/figures.py
@@ -1,24 +1,32 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 Window.clearcolor = (1, 1, 1, 1)
 
-class Ellipse(Widget):
+class Ans1(TextInput):
     pass
-class Circle(Widget):
+class Ans2(TextInput):
     pass
-class Rectangle(Widget):
+class Ans3(Label):
     pass
 
+class MyBL(BoxLayout):
+    def changeText(self):
+        self.ids.label.text = str(Ans1.text + Ans2.text)
+        return MyBL()
 
 class FiguresApp(App):
     def build(self):
         root = GridLayout(cols = 3, padding = 20, spacing = 20)
-        root.add_widget(Ellipse())
-        root.add_widget(Circle())
-        root.add_widget(Rectangle())
+        root.add_widget(Ans1())
+        root.add_widget(Ans2())
+        root.add_widget(Ans3())
         return root
+
     
 FiguresApp().run()
