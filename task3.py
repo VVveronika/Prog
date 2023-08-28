a50b6e348b0a93876b4b56f8ec1a0578799383ec
Veronika Vronskaya <vveronika1917@gmai.com>
2023-08-28 21:43:14 +0300

    Неверное решение

--- task3.py
+++ task3.py
@@ -1,8 +1,11 @@
 n = int(input())
-m = input().split(' ')
 
-for i in (1, n, 2):
-    if m[i] == m[i+1] or m[i] == m[i-1]:
-        print('No')
-    else:
-        print('Yes')
+sequence = list(map(float, input().split(' ')))
+sequence.sort(reverse=True)
+
+if sequence[n//2 - 1] > sequence[n//2]:
+    print('Yes')
+else:
+    print('No')
+
+
