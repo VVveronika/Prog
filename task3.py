9017fa04e4e2f5cc9c47a717bbd51f68629e49af
Veronika Vronskaya <vveronika1917@gmai.com>
2023-08-30 00:37:36 +0300

    Верное решение.

--- task3.py
+++ task3.py
@@ -1,11 +1,18 @@
 n = int(input())
 
 sequence = list(map(float, input().split(' ')))
-sequence.sort(reverse=True)
+sequence.sort()
 
-if sequence[n//2 - 1] > sequence[n//2]:
-    print('Yes')
-else:
-    print('No')
+answer = 'Yes'
+a = 0
 
+for i in range(n // 2 + 1, n):
+
+    if sequence[i] <= sequence[a] or sequence[i] <= sequence[a + 1]:
+        answer = 'No'
+        break
+
+    a += 1
+
+print(answer)
 
